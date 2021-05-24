ccb1283a8ee18a749c88f975c01fb93a4eb87b80
jyeon1039 <20181039@sungshin.ac.kr>
2021-05-24 16:27:32 +0900

    20210523_leetcode_17(jihyeon)

---
+++ leetcode/dfs, bfs/17(jihyeon).cpp
@@ -0,0 +1,24 @@
+﻿class Solution {
+public:
+    string letter[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    vector<string> result;
+    
+    void dfs(int digit, int index, string digits, string s){
+        if(s.length() == digits.length()){
+            result.push_back(s);
+            return;
+        }
+        
+        for(int i=0; i<letter[digit].length(); i++)
+            dfs(digits[index+1]-'0', index+1, digits, s+letter[digit][i]);
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits != "")
+            dfs(digits[0]-'0', 0, digits, "");
+        
+        return result;
+    }
+};
+​
