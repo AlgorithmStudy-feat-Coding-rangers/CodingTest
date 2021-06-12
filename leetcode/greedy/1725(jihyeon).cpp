fbff59cd7c6338b915c596420f7ce2eca1dccada
jyeon1039 <20181039@sungshin.ac.kr>
2021-06-12 23:09:10 +0900

    210606_leetcode_1725(jihyeon)

---
+++ leetcode/greedy/1725(jihyeon).cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        int max = 0;
+        vector<int> num;
+        int count = 0;
+        for(int i=0; i<rectangles.size(); i++){
+            int cur = min(rectangles[i][0], rectangles[i][1]);
+            num.push_back(cur);
+            
+            if(cur > max){
+                max = cur;
+                count = 0;
+            }
+            
+            if(cur == max)
+                count++;
+        }
+        
+        return count;
+    }
+};
